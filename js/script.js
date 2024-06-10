ba4eb4962975b12ae808d9e53882c82863607c14
Dmitry-Krtadze <mr.dima.kirtadze@gmail.com>
2024-06-10 21:51:07 +0300

    Changes to be committed: 	new file:   css/slick-theme.css 	new file:   css/slick.css 	modified:   index.html 	modified:   js/script.js 	new file:   js/slick.min.js 	new file:   photos/1.jpg 	new file:   photos/2.jpg 	new file:   photos/3.jpg 	new file:   photos/4.jpg 	new file:   photos/5.jpg 	modified:   sass/style.css 	modified:   sass/style.css.map 	modified:   sass/style.scss

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+
+    setTimeout(function() {
+        $('#preloader').animate({
+            opacity: 0,
+            width: '100%',
+            height: '100%',
+            marginTop: '0', // половина нової висоти
+            marginLeft: '0' // половина нової ширини
+        }, 500, function() {
+            $(this).remove(); // Видаляємо прелоадер після анімації
+        });
+    }, 500);
+
+
+
+
+
+
+
+
+
+
+
     $('#burgerMenu').on('click', function() {
         $('#navLinks').toggleClass('active');
     });
@@ -8,4 +32,65 @@ $(document).ready(function() {
         $('#servicesDropdown').toggleClass('active');
         $(this).find('.arrow-down').toggleClass('open');
     });
+
+
+
+
+
+    
+    function checkScroll() {
+        $('.services_block').each(function() {
+            if ($(this).isInViewport()) {
+                $(this).addClass('scrolled');
+            } else {
+                $(this).removeClass('scrolled');
+            }
+        });
+    }
+
+    $(window).on('scroll', checkScroll);
+    checkScroll(); // Initial check on page load
+
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+
+
+
+    
 });
+
+
+
+
+$('.slickSlider').slick({
+
+    slidesToShow: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
