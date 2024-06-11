9bb9f3f90dfb7708d4fd28a1e0d44e4beb06bb06
Dmitry-Krtadze <mr.dima.kirtadze@gmail.com>
2024-06-11 23:35:52 +0300

    Changes to be committed: 	new file:   aboba.cpp 	modified:   index.html 	modified:   js/script.js 	new file:   photos/AboutUs.jpg 	new file:   photos/icons/icons8-commodity-100.png 	new file:   photos/icons/icons8-handle-with-care-100.png 	new file:   photos/icons/icons8-ophthalmology-100.png 	new file:   photos/icons/icons8-palette-64.png 	new file:   photos/icons/icons8-shine-96.png 	modified:   sass/style.css 	modified:   sass/style.css.map 	modified:   sass/style.scss

--- js/script.js
+++ js/script.js
@@ -1,72 +1,93 @@
-$(document).ready(function() {
 
-
-    setTimeout(function() {
-        $('#preloader').animate({
-            opacity: 0,
-            width: '100%',
-            height: '100%',
-            marginTop: '0', // половина нової висоти
-            marginLeft: '0' // половина нової ширини
-        }, 500, function() {
-            $(this).remove(); // Видаляємо прелоадер після анімації
-        });
-    }, 500);
-
-
-
-
-
-
-
-
-
-
-
-    $('#burgerMenu').on('click', function() {
-        $('#navLinks').toggleClass('active');
+setTimeout(function() {
+    $('#preloader').animate({
+        opacity: 0,
+        width: '100%',
+        height: '100%',
+        marginTop: '0', // половина нової висоти
+        marginLeft: '0' // половина нової ширини
+    }, 500, function() {
+        $(this).remove(); // Видаляємо прелоадер після анімації
     });
+}, 500);
 
-    $('#servicesLink').on('click', function(e) {
-        e.preventDefault();
-        $('#servicesDropdown').toggleClass('active');
-        $(this).find('.arrow-down').toggleClass('open');
-    });
+$('#burgerMenu').on('click', function() {
+    $('#navLinks').toggleClass('active');
+});
 
-
-
-
-
-    
-    function checkScroll() {
-        $('.services_block').each(function() {
-            if ($(this).isInViewport()) {
-                $(this).addClass('scrolled');
-            } else {
-                $(this).removeClass('scrolled');
-            }
-        });
-    }
-
-    $(window).on('scroll', checkScroll);
-    checkScroll(); // Initial check on page load
-
-    $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
-        var elementBottom = elementTop + $(this).outerHeight();
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
-        return elementBottom > viewportTop && elementTop < viewportBottom;
-    };
-
-
-
-
-    
+$('#servicesLink').on('click', function(e) {
+    e.preventDefault();
+    $('#servicesDropdown').toggleClass('active');
+    $(this).find('.arrow-down').toggleClass('open');
 });
 
 
 
+
+
+
+
+
+
+
+    
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Створення Intersection Observer
+    const callback = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const block = entry.target;
+                const counter = block.querySelector('.counter');
+                const targetValue = counter.getAttribute('data-target');
+                const circle = block.querySelector('.circle');
+
+                circle.style.strokeDasharray = `0, 100`; 
+
+                setTimeout(() => {
+                    circle.style.transition = 'stroke-dasharray 2s ease';
+                    circle.style.strokeDasharray = `${targetValue}, 100`;
+                }, 100); 
+
+                counterUp(counter, {
+                    duration: 2000,
+                    delay: 16,
+                });
+
+                block.classList.add('is-visible');
+                observer.unobserve(block); // Зупинити спостереження за елементом після анімації
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(callback, { threshold: 0.5 });
+
+    document.querySelectorAll('.progress-block').forEach(block => {
+        observer.observe(block);
+    });
+});
+
+// Функція для анімації чисел
+function counterUp(el, options) {
+    const duration = options.duration || 1000;
+    const delay = options.delay || 16;
+    const target = parseInt(el.getAttribute('data-target'), 10);
+    let count = 0;
+    const increment = target / (duration / delay);
+
+    const updateCounter = () => {
+        count += increment;
+        if (count > target) {
+            el.textContent = target;
+        } else {
+            el.textContent = Math.round(count);
+            setTimeout(updateCounter, delay);
+        }
+    };
+
+    updateCounter();
+}
 
 $('.slickSlider').slick({
 
