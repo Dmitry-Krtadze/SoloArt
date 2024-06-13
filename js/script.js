4b034832bef3a535d46958e90046d6454b212c74
Dmitry-Krtadze <mr.dima.kirtadze@gmail.com>
2024-06-13 13:53:13 +0300

    modified:   index.html 	modified:   js/script.js 	new file:   js/slick.js 	modified:   sass/style.css 	modified:   sass/style.css.map 	modified:   sass/style.scss 	new file:   video/video.mp4 	modified:   wall_painting.html

--- js/script.js
+++ js/script.js
@@ -115,31 +115,3 @@ window.onclick = function(event) {
     }
 }
 
-
-$('.slickSlider').slick({
-
-    slidesToShow: 3,
-    dots: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
---
+++ js/slick.js
@@ -0,0 +1,53 @@
+
+$('.slickSlider').slick({
+
+    slidesToShow: 3,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    responsive: [
+
+        {
+          breakpoint: 480,
+          settings: {
+            dots: true,
+          }
+        }
+      ]
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    arrows: false,
+    focusOnSelect: true
+  });
